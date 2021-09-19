7f6adb56d3e8f7840fa8c59dc984012080575c09
Jordan Scott <jscott0751@gmail.com>
2021-09-18 20:17:34 -0400

    ran tables successfully in sql

--- schema.sql
+++ schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE department(
     id INT PRIMARY KEY,
     name VARCHAR(30) 
 
-),
+);
 
 CREATE TABLE role(
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
-),
+);
 
 CREATE TABLE employee(
     id INT PRIMARY KEY,
@@ -23,4 +23,4 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
-)
+);
